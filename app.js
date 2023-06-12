7976dea7792f24a9b8b33f178856cb261c68bbe3
Nico <lopeznnr@gmail.com>
2023-06-12 09:57:26 -0300

    escribi variables en js

---
+++ app.js
@@ -0,0 +1,18 @@
+// Variables
+
+const btnCierra = document.getElementById("btnCierra")
+
+const btnAdelanta = document.getElementById("btnAdelanta")
+
+const btnRetrocede = document.getElementById("btnRetrocede")
+
+const imagenes = document.querySelectorAll("#galeriaImg")
+
+const lightBox = document.getElementById("contenedorPrincipal")
+
+const imgActiva = document.getElementById("imagenActiva")
+
+let imagenIndice = 0
+
+// Abrir el lightbox
+
