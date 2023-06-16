e021992cb0aa05f5670779f4474cc091774e91b3
Nico <lopeznnr@gmail.com>
2023-06-16 16:52:21 -0300

    funcion abrir y cerrar lightbox funcionando

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const btnAdelanta = document.getElementById("btnAdelanta")
 
 const btnRetrocede = document.getElementById("btnRetrocede")
 
-const imagenes = document.getElementById("imagenes")
-
+const imagenes = document.getElementsByClassName("imagenes")
+console.log(imagenes)
 const lightBox = document.getElementById("contenedorPrincipal")
 
 const imgActiva = document.getElementById("imagenActiva")
@@ -16,17 +16,30 @@ let imagenIndice = 0
 
 // Abrir el lightbox
 
-const abrir = () => {
+const abrir = (event) => {
    lightBox.style.display = "flex"
+   imgActiva.src = event.target.src
+   console.log(event)
+   console.log(event.target.src)
 }
 
-imagenes.addEventListener("click", abrir)
+for(let imagen of imagenes){
+    imagen.addEventListener('click', (event) => abrir(event))
+}
 
+// Cerrar el lightbox
 
 const cerrar = () => {
     lightBox.style.display = "none"
+
 }
 
 btnCierra.addEventListener("click", cerrar)
 
+// Cambiar imagenes
 
+/* arrary de imagenes
+indices de de 0 a 9,
+variable con index de imgactiva
+array[index]
+*/
